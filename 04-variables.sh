7f6e174179562e9e4001cc26ce8fb2baab476d09
raghathanuja <raghathanujapv@gmail.com>
2023-12-14 15:49:06 +0530

    added date

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: This is $PERSON1, How are you?"
+echo "$PERSON2: Hi $PERSON1, Glad to meet you?"
+echo "$PERSON1: Nice to meet you too"
+echo "$PERSON2: Have a good day"

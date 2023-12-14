631f9331d0384a0a8c46f80429a0f0f311755cca
raghathanuja <raghathanujapv@gmail.com>
2023-12-14 13:42:21 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ragha: This is Ragha, How are you?"
-echo "Venkat: Hi Thanu, Glad to meet you?"
-echo "Ragha: Nice to meet you too"
-echo "Venkat: Have a good day"
+PERSON1=Ragha
+PERSON2=Venkat
+
+echo "$PERSON1: This is $PERSON1, How are you?"
+echo "$PERSON2: Hi $PERSON1, Glad to meet you?"
+echo "$PERSON1: Nice to meet you too"
+echo "$PERSON2: Have a good day"

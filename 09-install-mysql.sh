fa083171a0de41e60590ec59d322a4c158cafa80
raghathanuja <raghathanujapv@gmail.com>
2023-12-14 23:07:52 +0530

    conditions given

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -2,10 +2,9 @@
 
 ID=$(id-u)
 
-if[ $ID -ne 0 ];
+if [ $ID -ne 0 ]
 then
     echo "ERROR: : Please run this script with access"
 else
-    echo "You are root user"   
-
+    echo "You are root user"
 fi    

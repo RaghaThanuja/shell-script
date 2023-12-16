5828b6f8f3af2c9a0d595611144cf6910fbc1f2b
raghathanuja <raghathanujapv@gmail.com>
2023-12-16 16:24:22 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,16 +6,15 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR: : $2 ....$R FAILED $N"
+        echo -e "ERROR: : $2 ... $R FAILED $N"
         exit 1    
     else
-        echo "$2 ....$G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi    
 }
 
